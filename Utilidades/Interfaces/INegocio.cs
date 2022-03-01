83b57ff13b7f9733d694069269f9e751ef78d22a
Rugama27 <94814548+Rugama27@users.noreply.github.com>
2022-03-01 14:43:25 -0600

    Add files via upload

---
+++ Utilidades/Interfaces/INegocio.cs
@@ -0,0 +1,25 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilidades.Interfaces
+{
+    public interface INegocio <entity>
+    {
+        //Es un contrato con los objetos que la incrementan
+        //Solamente tendremos las firmas de los métodos.
+
+        entity save(entity cliente);
+
+        entity update(entity cliente);
+
+        bool delete(entity cliente);
+
+        List<entity> getAll();
+
+        entity getById(int id);
+    }
+}
