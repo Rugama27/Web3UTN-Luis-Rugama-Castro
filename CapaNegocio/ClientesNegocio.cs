bc124bf87613fdfdbddbf4fd51060ac7e0d0aec8
Rugama27 <94814548+Rugama27@users.noreply.github.com>
2022-03-01 14:41:44 -0600

    Add files via upload

---
+++ CapaNegocio/ClientesNegocio.cs
@@ -0,0 +1,46 @@
+﻿using CapaDatos;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilidades.Interfaces;
+
+namespace CapaNegocio
+{
+    public class ClientesNegocio : INegocio <clsCliente>
+    {
+        public IDatos<clsCliente> ClienteDatos { get; }
+
+        public ClientesNegocio(IDatos<clsCliente> _clientesDatos)
+        {
+            ClienteDatos = _clientesDatos;
+        }
+
+        public bool delete(clsCliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<clsCliente> getAll()
+        {
+            return ClienteDatos.getAll();
+        }
+
+        public clsCliente getById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public clsCliente save(clsCliente cliente)
+        {
+            return ClienteDatos.save(cliente);
+        }
+
+        public clsCliente update(clsCliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
