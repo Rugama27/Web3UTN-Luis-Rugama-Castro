f7c146d293fcde2915a3f7cb762dbede1930e023
Rugama27 <94814548+Rugama27@users.noreply.github.com>
2022-03-08 14:35:33 -0600

    Add files via upload

---
+++ CapaPresentacion/frmClientes.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Utilidades.Enumeradores;
+using Entidades;
+using CapaNegocio;
+using Utilidades.Interfaces;
+using Utilidades.Exceptions;
+using Utilidades;
+
+namespace CapaPresentacion
+{
+    public partial class frmClientes : Form
+    {
+        public clsCliente cliente { get; set; }
+
+        //Acá se manda a llamar la capa de negocio de cliente - guardar
+        public INegocio<clsCliente> ClienteNegocio { get; }
+
+        public frmClientes(INegocio<clsCliente> _clienteNegocio)
+        {
+            InitializeComponent();
+            ClienteNegocio = _clienteNegocio;
+        }
+
+        private void pcbClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            bool isCorrect = validarDatos();
+
+            try
+            {
+                //Permite validar el formulario y da paso a guardar
+                if (isCorrect)
+                {
+                    //New desde 0
+                    clsCliente cliente = new clsCliente();
+                    cliente.identificacion = txtIdentificacion.Text.Trim().ToUpper();
+                    cliente.tipoId = (int)cmbTipoId.SelectedValue;
+                    cliente.nombre = txtNombre.Text.Trim().ToUpper();
+                    cliente.apellido1 = txtApell1.Text.Trim().ToUpper();
+                    cliente.apellido2 = txtApell2.Text.Trim().ToUpper();
+                    cliente.tipoCliente = (int)cmbTipoCliente.SelectedValue;
+                    cliente.fecha_socio = dtpFechaSocio.Value;
+                    cliente.estado = true;
+
+                    ClienteNegocio.save(cliente);
+                    MessageBox.Show("Cliente guardado", "Guardar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    this.Close();
+                }
+            }
+            catch (EntityExistException ex)
+            {
+                MessageBox.Show(ex.Message, "Guardar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se pudo guardar, contacte el Administrador", "Guardar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void frmClientes_Load(object sender, EventArgs e)
+        {
+            if (cliente == null)
+            {
+                lblTitulo.Text = "Crear Cliente";
+            }
+            else
+            {
+                lblTitulo.Text = "Modificar Cliente";
+                cargarForm();
+                txtIdentificacion.Enabled = false;
+                cmbTipoId.Enabled = false;
+            }
+            
+
+            cargarCombos();
+
+            cmbTipoCliente.SelectedIndex = 0;
+            cmbTipoId.SelectedIndex = 0;
+        }
+
+        private void cargarForm()
+        {
+            txtIdentificacion.Text = cliente.identificacion;
+            cmbTipoId.Text = Enum.GetName(typeof(Enumeradores.tipoId), cliente.tipoId);
+            txtNombre.Text = cliente.nombre;
+            txtApell1.Text = cliente.apellido1;
+            txtApell2.Text = cliente.apellido2;
+            cmbTipoCliente.Text = Enum.GetName(typeof(Enumeradores.tipoCliente), cliente.tipoCliente);
+            dtpFechaSocio.Value = cliente.fecha_socio;
+        }
+
+        private void cargarCombos()
+        {
+            cmbTipoId.DataSource = Enum.GetValues(typeof(tipoId));
+            cmbTipoCliente.DataSource = Enum.GetValues(typeof(tipoCliente));
+        }
+
+        private bool validarDatos()
+        {
+            if (txtIdentificacion.Text == string.Empty)
+            {
+                MessageBox.Show("Falta la Identificación","Faltan Datos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (txtNombre.Text == string.Empty)
+            {
+                MessageBox.Show("Falta el Nombre", "Faltan Datos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (txtApell1.Text == string.Empty)
+            {
+                MessageBox.Show("Falta el Primer Apellido", "Faltan Datos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (txtApell2.Text == string.Empty)
+            {
+                MessageBox.Show("Falta el Segundo Apellido", "Faltan Datos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
+
+        private void lblTitulo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbTipoId_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CapaPresentacion/frmListaClientes.cs
@@ -0,0 +1,101 @@
+﻿using CapaNegocio;
+using Entidades;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Utilidades.Interfaces;
+
+namespace CapaPresentacion
+{
+    public partial class frmListaClientes : Form
+    {
+        List<clsCliente> lista;
+
+        public INegocio<clsCliente> ClienteNegocio { get; }
+        public IServiceProvider ServiceProvider { get; }
+
+        public frmListaClientes(INegocio<clsCliente> _clienteNegocio, IServiceProvider serviceProvider)
+        {
+            InitializeComponent();
+            ClienteNegocio = _clienteNegocio;
+            ServiceProvider = serviceProvider;
+        }
+
+        private void frmClientes_Load(object sender, EventArgs e)
+        {
+            lblTitulo.Text = "Mantenimiento de Clientes";
+
+            obtenerDatos();
+        }
+
+        public void obtenerDatos()
+        {
+            try
+            {
+                lista = ClienteNegocio.getAll();
+                cargarListView(lista);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void cargarListView(List<clsCliente> lista)
+        {
+            lstVista.Items.Clear();
+
+            foreach (var cliente in lista)
+            {
+                ListViewItem item = new ListViewItem();
+                item.Text = cliente.identificacion;
+                item.SubItems.Add(cliente.nombre.ToUpper());
+                item.SubItems.Add(cliente.apellido1.ToUpper());
+                item.SubItems.Add(cliente.apellido2.ToUpper());
+                lstVista.Items.Add(item);
+            }
+        }
+
+        private void pcbClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            var form = ServiceProvider.GetRequiredService<frmClientes>();
+            form.ShowDialog();
+
+            obtenerDatos();
+        }
+
+        private void lstVista_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                var ident = lstVista.SelectedItems[0].Text;
+                var cliente = lista.Where(x => x.identificacion.Trim().Equals(ident.Trim())).SingleOrDefault();
+                var form = ServiceProvider.GetRequiredService<frmClientes>();
+                form.cliente = cliente;
+                form.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se pudo cargar el cliente, contacte el Administrador", "Guardar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void pnlTitulo_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
