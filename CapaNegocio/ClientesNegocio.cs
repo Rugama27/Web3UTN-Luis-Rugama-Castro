ae62888eb6a852ff9f29148911114be88f317fc6
Rugama27 <94814548+Rugama27@users.noreply.github.com>
2022-03-18 10:55:43 -0600

    Semana 8

---
+++ CapaNegocio/ClientesNegocio.cs
@@ -0,0 +1,88 @@
+﻿using CapaDatos;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilidades.Exceptions;
+using Utilidades.Interfaces;
+
+namespace CapaNegocio
+{
+    public class ClientesNegocio : INegocio <clsCliente>
+    {
+        public IDatos<clsCliente> ClienteDatos { get; }
+
+        public ClientesNegocio(IDatos<clsCliente> _clientesDatos)
+        {
+            ClienteDatos = _clientesDatos;
+        }
+
+        public bool delete(clsCliente cliente)
+        {
+            try
+            {
+                return ClienteDatos.delete(cliente);    
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            throw new NotImplementedException();
+        }
+
+        public List<clsCliente> getAll()
+        {
+            return ClienteDatos.getAll();
+        }
+
+        public clsCliente getById(int id)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public clsCliente save(clsCliente cliente)
+        {
+            //Reglas de negocio
+            //Se valida que el cliente no exista mediante su Id
+            try
+            {
+                var cli = ClienteDatos.getByIdent(cliente.identificacion);
+                if (cli != null)
+                {
+                    throw new EntityExistException("Cliente");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return ClienteDatos.save(cliente);
+        }
+
+        public clsCliente update(clsCliente cliente)
+        {
+            try
+            {
+                return ClienteDatos.update(cliente);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+    }
+}
