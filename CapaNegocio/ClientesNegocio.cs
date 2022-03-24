beaadf23eede4e93f3ffb21887eb139fe118366e
Rugama27 <94814548+Rugama27@users.noreply.github.com>
2022-03-24 15:35:59 -0600

    Semana 9

---
+++ CapaNegocio/ClientesNegocio.cs
@@ -0,0 +1,90 @@
+﻿using CapaDatos;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilidades.Exceptions;
+using Utilidades.Interfaces;
+
+namespace CapaNegocio
+{
+    public class ClientesNegocio : INegocio <tbCliente>
+    {
+        public IDatos<tbCliente> ClienteDatos { get; }
+
+        public ClientesNegocio(IDatos<tbCliente> _clientesDatos)
+        {
+            ClienteDatos = _clientesDatos;
+        }
+
+        public bool delete(tbCliente cliente)
+        {
+            try
+            {
+                return ClienteDatos.delete(cliente);    
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            throw new NotImplementedException();
+        }
+
+        public List<tbCliente> getAll()
+        {
+            return ClienteDatos.getAll();
+        }
+
+        public tbCliente getById(string id)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public tbCliente save(tbCliente cliente)
+        {
+            //Reglas de negocio
+            //Se valida que el cliente no exista mediante su Id
+            try
+            {
+                var cli = ClienteDatos.getById(cliente.id);
+                if (cli != null)
+                {
+                    throw new EntityExistException("Cliente");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return ClienteDatos.save(cliente);
+        }
+
+        public tbCliente update(tbCliente cliente)
+        {
+            try
+            {
+                return ClienteDatos.update(cliente);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+
+       
+    }
+}
